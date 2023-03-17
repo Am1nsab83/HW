3b245885a5098c5daf73d076683eeccb238b15d7
Am1nsab83 <amnsabet83@gmail.com>
2023-03-17 23:32:40 +0330

    modified:   2/12.c 	modified:   2/2.MD 	deleted:    2/a.out 	modified:   2/final.txt

--- 2/12.c
+++ 2/12.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    
+    const float sa=3.156;
+    int a;
+    float f;
+    printf("please insert your age (only in years)\n:");
+    scanf("%d",&a);
+    f=a*sa;
+    printf("your age in seconds is equel to:%f * 10^7",f);
+    return 0;
 }
